02b58e55417ad37038d3abb16b256563261761fb
吉本和弘 <kazuy.dj9@gmail.com>
2023-10-01 13:19:57 +0900

    commit

---
+++ src/main/kotlin/com/example/kotlinwebfluxsample2/KotlinWebfluxSample2Application.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinwebfluxsample2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinWebfluxSample2Application
+
+fun main(args: Array<String>) {
+    runApplication<KotlinWebfluxSample2Application>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlinwebfluxsample2/KotlinWebfluxSample2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinwebfluxsample2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinWebfluxSample2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
